3b8e5ab873f49da407937ea13877aa61104c19d2
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-10-15 12:10:07 +0100

    Use jinja2 template

--- make_status.py
+++ make_status.py
@@ -1,44 +1,6 @@
 import subprocess
 import shutil
-
-TEMPLATE_ROW = """
-  <tr>
-    <td>
-      {display_name}
-    </td>
-    <td>
-      <a href="https://travis-ci.org/{user}/{package_name}">
-        <img src="https://travis-ci.org/{user}/{package_name}.png?branch=master">
-      </a>
-    </td>
-    <td>
-      <a href="https://coveralls.io/r/{user}/{package_name}?branch=master">
-        <img src="https://coveralls.io/repos/{user}/{package_name}/badge.png?branch=master">
-      </a>
-    </td>
-    <td>
-      <a href="https://readthedocs.org/projects/{package_name}/?badge=latest">
-        <img src="https://readthedocs.org/projects/{package_name}/badge/?version=latest">
-      </a>
-    </td>
-  </tr>
-"""
-
-TEMPLATE_TABLE = """
-<html>
-  <head>
-    <link href='http://www.astropy.org/css/style.css' rel='stylesheet' type="text/css">
-  </head>
-  <body>
-    <div id="wrapper">
-    <h1>Astropy Project Dashboard</h1>
-    <table align='center'>
-      {rows}
-    </table>
-    </div>
-  </body>
-</html>
-"""
+from jinja2 import Template
 
 PROJECTS = ['astropy/astropy', 'astrofrog/reproject', 'astrofrog/wcsaxes',
             'aplpy/aplpy', 'astropy/montage-wrapper', 'astropy/ccdproc',
@@ -47,16 +9,20 @@ PROJECTS = ['astropy/astropy', 'astrofrog/reproject', 'astrofrog/wcsaxes',
 
 rows = ""
 
-for project in PROJECTS:
-    user, repo = project.split('/')
-    rows += TEMPLATE_ROW.format(package_name=repo, display_name=project, user=user)
+packages = []
 
-content = TEMPLATE_TABLE.format(rows=rows)
+for package_url in PROJECTS:
+    package = {}
+    package['display_name'] = package_url
+    package['user'], package['name'] = package_url.split('/')
+    packages.append(package)
+
+template = Template(open('template.html', 'r').read())
 
 subprocess.call('git checkout gh-pages', shell=True)
 
 with open('status.html', 'w') as f:
-    f.write(content)
+    f.write(template.render(packages=packages))
 
 subprocess.call('git add status.html', shell=True)
 subprocess.call('git commit -m "Latest build"', shell=True)
