47c8f8a9873402055d31edec008194398adf93ab
Julia Signell <jsignell@gmail.com>
2019-02-05 15:16:49 -0500

    Updating with intake info and adding different channels

--- make_status.py
+++ make_status.py
@@ -11,19 +11,19 @@ with open('dashboard.yml') as f:
 existing = {package['repo'].split('/')[1].lower(): package for section in config for package in section['packages']}
 
 # Also get affiliated packages
-registry = requests.get('http://www.astropy.org/affiliated/registry.json').json()['packages']
-for section in config:
-    if section['name'] == 'Affiliated Packages':
-        affiliated = section
-        break
-else:
-    print("Could not find affiliated package section in dashboard.yml")
-    sys.exit(1)
+registry = []
+# for section in config:
+#     if section['name'] == 'Affiliated Packages':
+#         affiliated = section
+#         break
+# else:
+#     print("Could not find affiliated package section in dashboard.yml")
+#     sys.exit(1)
 
 for package in registry:
     # FIXME: Not all repo name is actual package name.
     pkg_name = package['name'].lower()
-    
+
     if pkg_name in existing:
         entry = existing[pkg_name]
     else:
@@ -37,16 +37,17 @@ for package in registry:
         entry['pypi_name'] = package['pypi_name']
     if 'badges' not in entry:
         entry['badges'] = 'travis, coveralls, rtd, pypi, conda'
-    if pkg_name not in existing:
-        affiliated['packages'].append(entry)
+    # if pkg_name not in existing:
+    #     affiliated['packages'].append(entry)
 
 for section in config:
     for package in section['packages']:
         package['user'], package['name'] = package['repo'].split('/')
         package['badges'] = [x.strip() for x in package['badges'].split(',')]
-        if 'rtd_name' not in package:
+        package['conda_package'] = package.get('conda_package', package['name'])
+        if 'rtd' in package['badges'] and 'rtd_name' not in package:
             package['rtd_name'] = package['name']
-        if 'pypi_name' not in package:
+        if 'pypi' in package['badges'] and 'pypi_name' not in package:
             package['pypi_name'] = package['name']
         if 'appveyor' in package['badges'] and 'appveyor_project' not in package:
             package['appveyor_project'] = package['repo']
@@ -54,10 +55,10 @@ for section in config:
             package['circleci_project'] = package['repo']
         if 'travis' in package['badges'] and 'travis_project' not in package:
             package['travis_project'] = package['repo']
-        if 'conda' in package['badges'] and 'conda_project' not in package:
-            package['conda_project'] = 'astropy/' + package['name']
+        if 'conda' in package['badges'] and 'conda_channel' not in package:
+            package['conda_channel'] = 'intake'
 
-affiliated['packages'] = sorted(affiliated['packages'], key=lambda x: x['name'].lower())
+# affiliated['packages'] = sorted(affiliated['packages'], key=lambda x: x['name'].lower())
 
 template = Template(open('template.html', 'r').read())
 
