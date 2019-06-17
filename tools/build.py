ca29410e0f42151682311f979c879751e0bebde0
Julia Signell <jsignell@gmail.com>
2019-06-17 14:35:53 -0400

    Adding sponsor info

--- tools/build.py
+++ tools/build.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
 import os
-import sys
 from jinja2 import Template
 from yaml import safe_load
-import requests
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -12,27 +10,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'tools.yml')) as f:
     config = safe_load(f)
 
-existing = {package['repo'].split('/')[1].lower(): package for section in config for package in section['packages']}
-
-registry = []
-
-for package in registry:
-    # FIXME: Not all repo name is actual package name.
-    pkg_name = package['name'].lower()
-
-    if pkg_name in existing:
-        entry = existing[pkg_name]
-    else:
-        entry = {}
-    if 'repo' not in entry:
-        if 'github.com' in package['repo_url']:
-            entry['repo'] = package['repo_url'].split('github.com/')[1]
-        else:
-            print("Skipping package {0} which is not on GitHub".format(package['name']))
-    if 'pypi_name' not in entry:
-        entry['pypi_name'] = package['pypi_name']
-    if 'badges' not in entry:
-        entry['badges'] = 'coveralls, rtd, pypi, conda'
+with open(os.path.join(here, 'sponsors.yml')) as f:
+    sponsors = safe_load(f)
 
 for section in config:
     for package in section['packages']:
@@ -49,7 +28,8 @@ for section in config:
             package['site'] = '{}.org'.format(package['name'])
         if 'site' in package['badges'] and 'site_protocol' not in package:
             package['site_protocol'] = 'https'
-
+        if package.get('sponsors'):
+            package['badges'].append('sponsor')
 
 template = Template(open(os.path.join(here, 'template.html'), 'r').read())
 
@@ -57,4 +37,4 @@ with open(os.path.join(here, 'index.rst'), 'w') as f:
     f.write("All Tools\n")
     f.write("=========\n\n")
     f.write(".. raw:: html\n\n")
-    f.write(template.render(config=config))
+    f.write(template.render(config=config, sponsors=sponsors))
