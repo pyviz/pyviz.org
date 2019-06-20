2867025b3db4af67373f5a500ceaba27952b77f3
Julia Signell <jsignell@gmail.com>
2019-06-20 11:41:31 -0400

    Adding caching mechnism and dormant badge

--- tools/build.py
+++ tools/build.py
@@ -4,21 +4,36 @@ import os
 from jinja2 import Template
 from yaml import safe_load
 from markdown import markdown
+import requests
 
 
 here = os.path.abspath(os.path.dirname(__file__))
+cache_path = os.path.join(here, '..', 'doc', '_static', 'cache')
+build_cache = eval(os.getenv('BUILD_CACHE', 'False'))
+
+cache = {
+    "stars": "https://img.shields.io/github/stars/{repo}.svg?style=social",
+    "contributors": "https://img.shields.io/github/contributors/{repo}.svg?style=social&logo=github",
+    "downloads": "https://img.shields.io/pypi/dd/{pypi_name}.svg?logo=pypi",
+    "license": "https://img.shields.io/pypi/l/{pypi_name}.svg",
+}
+
+if build_cache:
+    if not os.path.exists(cache_path):
+        os.mkdir(cache_path)
 
 with open(os.path.join(here, 'tools.yml')) as f:
     config = safe_load(f)
-
-with open(os.path.join(here, 'sponsors.yml')) as f:
-    sponsors = safe_load(f)
 
 for section in config:
     if section.get('intro'):
         section['intro'] = markdown(section['intro'])
     for package in section['packages']:
-        package['user'], package['name'] = package['repo'].split('/')
+        try:
+            package['user'], package['name'] = package['repo'].split('/')
+        except:
+            raise Warning('Package.repo is not in correct format', package)
+            continue
         package['conda_package'] = package.get('conda_package', package['name'])
 
         if package.get('badges'):
@@ -31,6 +46,8 @@ for section in config:
             package['badges'].append('sponsor')
         if package.get('site') and 'site' not in package['badges']:
             package['badges'].append('site')
+        if package.get('dormant') and 'dormant' not in package['badges']:
+            package['badges'].append('dormant')
 
         if 'rtd' in package['badges'] and 'rtd_name' not in package:
             package['rtd_name'] = package['name']
@@ -44,6 +61,15 @@ for section in config:
             if 'site_protocol' not in package:
                 package['site_protocol'] = 'https'
 
+        if build_cache:
+            for badge, url in cache.items():
+                rendered_url = url.format(repo=package['repo'], pypi_name=package.get('pypi_name', package['name']))
+                r = requests.get(rendered_url)
+                with open(os.path.join(cache_path, f"{package['name']}_{badge}_badge.svg"), 'wb') as f:
+                    f.write(r.content)
+
+with open(os.path.join(here, 'sponsors.yml')) as f:
+    sponsors = safe_load(f)
 
 template = Template(open(os.path.join(here, 'template.html'), 'r').read())
 
